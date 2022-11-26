cdb49d1887c41f79b862d654cf0f6bd4306f2bf5
nyuu posts <nyuutsu@protonmail.com>
2022-11-26 14:08:22 -0500

    general improvements

--- guessing_game.py
+++ guessing_game.py
@@ -5,22 +5,26 @@ import string
 import time
 
 class GameManager:
+  """A round of the game "Concentration"
+
+  To operate, call, in order:
+    constructor
+    boot()
+    game_loop()
+  """
   def __init__(self, width=500, height=500, squares_count=16, gap=50):
-    # initialize arg constants
+    # arg constants
     self.WIDTH = width # window width
     self.HEIGHT = width # window height
-
     self.SQUARES_COUNT = squares_count
     self.GAP = gap
-    
-    # initialize computed constants  
+    # computed constants  
     self.HALF_GAP = self.GAP // 2
     self.SQUARES_PER_ROW = int(math.floor(math.sqrt(self.SQUARES_COUNT)))
     self.MAX_SQUARE_WIDTH = self.WIDTH // self.SQUARES_PER_ROW
     self.HALF_SQUARE_WIDTH = self.MAX_SQUARE_WIDTH // 2
     self.SQUARES = [] # should this be CONSTANTCAPPED or no
-
-    # initialize hard constants
+    # more constants
     self.FONT_SIZE = 30
     self.HALF_FONT_SIZE = self.FONT_SIZE // 2
     self.FONT_FACE = 'courier'
@@ -34,73 +38,77 @@ class GameManager:
     # with correct play you only need 0.8n cards of guesses
     # https://www.tandfonline.com/doi/abs/10.4169/amer.math.monthly.120.09.787
     self.MAX_GUESSES = int(math.ceil(self.SQUARES_COUNT * 0.8))
-    self.remaining_guesses = self.MAX_GUESSES
+    self.remaining_guesses = self.MAX_GUESSES 
     self.FAIL_COOLDOWN = 1.5 # seconds
 
+    """
     # sanity check
-    # assert self.MAX_GUESSES == 13 # not true for all n, is true for n=16
     assert width == height
     assert width % 2 == 0
     assert gap % 2 == 0
     assert self.SQUARES_COUNT % 2 == 0
     assert math.sqrt(self.SQUARES_COUNT) == int(self.SQUARES_PER_ROW)
-    
-    # helper for create_one_square()
-    def gen_secrets(squares_count):
-      secrets = []
-      for char in string.ascii_uppercase:
-        secrets.append(char)
-        secrets.append(char) # we want these to come in pairs. better way?
-      truncated_secrets = secrets[0:squares_count]
-      #random.shuffle(truncated_secrets) #turn off shuffle to test matching
-      return iter(truncated_secrets)
-    self.secrets = gen_secrets(self.SQUARES_COUNT)
-    
-    # helper for create_one_row()
-    def create_one_square(p1, p2):
-      square = Rectangle(p1, p2)
-      square.setOutline(self.SQ_STROKE)
-      square.setFill(self.SQ_COLOR)
-      square.setWidth(self.SQ_STROKE_WIDTH)
-      secret = next(self.secrets)
-      self.SQUARES.append((square, secret))
+    """
 
-    # helper for gen_squares()
-    def create_one_row(quantity, width, displacement = 0):
-      x1 = 0
-      y1 = displacement
-      x2 = width
-      y2 = displacement + width
+  def boot(self):
+    self.secrets = self.gen_secrets(self.SQUARES_COUNT)
+    self.gen_squares()
+    self.create_window()
+    self.draw_squares()
 
-      for item in range(quantity):
-        create_one_square(Point(x1 + self.HALF_GAP, y1 + self.HALF_GAP), Point(x2 - self.HALF_GAP, y2 - self.HALF_GAP))
-        x1 += width
-        x2 += width
+  # helper for create_one_square()
+  @classmethod
+  def gen_secrets(cls, squares_count):
+    secrets = []
+    for char in string.ascii_uppercase:
+      secrets.append(char)
+      secrets.append(char) # we want these to come in pairs. better way?
+    truncated_secrets = secrets[0:squares_count]
+    #random.shuffle(truncated_secrets) #turn off shuffle to test matching
+    return iter(truncated_secrets)
+  
+  # helper for create_one_row()
+  def create_one_square(self, p1, p2):
+    square = Rectangle(p1, p2)
+    square.setOutline(self.SQ_STROKE)
+    square.setFill(self.SQ_COLOR)
+    square.setWidth(self.SQ_STROKE_WIDTH)
+    secret = next(self.secrets)
+    self.SQUARES.append((square, secret))
 
-    def gen_squares():
-      displacement = 0
-      for row in range(self.SQUARES_PER_ROW):
-        create_one_row(self.SQUARES_PER_ROW, self.MAX_SQUARE_WIDTH, displacement)
-        displacement += self.MAX_SQUARE_WIDTH
-    gen_squares()
+  # helper for gen_squares()
+  def create_one_row(self, quantity, width, displacement = 0):
+    x1 = 0
+    y1 = displacement
+    x2 = width
+    y2 = displacement + width
 
-    def create_window():
-      self.WINDOW = GraphWin("My Window", self.WIDTH, self.HEIGHT)
-      self.WINDOW.setBackground(self.BG_COLOR)
-    create_window()
+    for item in range(quantity):
+      self.create_one_square(Point(x1 + self.HALF_GAP, y1 + self.HALF_GAP), Point(x2 - self.HALF_GAP, y2 - self.HALF_GAP))
+      x1 += width
+      x2 += width
 
-    def draw_squares():
-      for obj in self.SQUARES:
-        obj[0].draw(self.WINDOW)
-    draw_squares()
+  def gen_squares(self):
+    displacement = 0
+    for row in range(self.SQUARES_PER_ROW):
+      self.create_one_row(self.SQUARES_PER_ROW, self.MAX_SQUARE_WIDTH, displacement)
+      displacement += self.MAX_SQUARE_WIDTH
+  
+  def create_window(self):
+    self.WINDOW = GraphWin("My Window", self.WIDTH, self.HEIGHT)
+    self.WINDOW.setBackground(self.BG_COLOR)
+  
+  def draw_squares(self):
+    for obj in self.SQUARES:
+      obj[0].draw(self.WINDOW)
 
   # takes (Rectangle, string)
   # notices where you clicked and draws the corresponding secret (tuple[1]) in a Text at the center of the containing square (tuple[0])
   # returns the Text
-  def reveal_and_return(self, data):
+  def reveal_and_return(self, rect_secret_tuple):
     # get the text we're printing
-    center = data[0].getCenter()
-    text = Text(center, data[1])
+    center = rect_secret_tuple[0].getCenter()
+    text = Text(center, rect_secret_tuple[1])
     text.setSize(self.FONT_SIZE)
     text.setFace(self.FONT_FACE)
     text.setTextColor(self.BG_COLOR)
@@ -133,7 +141,7 @@ class GameManager:
   # returns that tuple
   # I tried reworking this and am_in_square to use a decorator since they're like 99% the same function but I didn't figure it out and moved on. revisit!
   def contents_of_square(self, data):
-      # can I get out of having to write all this out?
+      # can I get out of having to write all this out AGAIN
       click_x = data.getX()
       click_y = data.getY()
 
@@ -173,18 +181,18 @@ class GameManager:
   def meditate(self):
     time.sleep(self.FAIL_COOLDOWN)
 
-  def generate_gutter_text(self, text, gutter_constant):
+  # reasonable values to use to decenter buttons: 0.5, 1.5
+  def generate_gutter_text(self, text, gutter_factor, horizontal_factor = 1):
     text = Text(\
       Point(\
-        int(math.ceil(self.WIDTH / 2)), \
-        int(math.ceil(self.HEIGHT * (0.25 * gutter_constant)))), \
+        int(math.ceil((self.WIDTH / 2) * horizontal_factor)), \
+        int(math.ceil(self.HEIGHT * (0.25 * gutter_factor)))), \
         text)
     text.setTextColor(self.SQ_COLOR)
     text.setSize(self.FONT_SIZE)
     return text
 
   def game_loop(self):
-    
     title_text = self.generate_gutter_text(f'{self.SQUARES_COUNT} tiles! Match them!', 1)
     title_text.draw(self.WINDOW)
     # while user has guesses and guessables
@@ -197,7 +205,7 @@ class GameManager:
       square_one = self.get_first_square()
       text_one = self.reveal_and_return(square_one)
       square_one[0].setOutline(self.MAYBE_STROKE)
-      # add a fail timer?
+      
       # prompt for a valid other guess; reveal
       square_two = self.get_second_square(square_one)
       text_two = self.reveal_and_return(square_two)
@@ -233,19 +241,34 @@ class GameManager:
         text_two.undraw()
       self.remaining_guesses -= 1
       guesses_text.undraw()
-      #print(f'remaining guesses: {self.remaining_guesses}')
-    # if either loop conditions are broken then we can identify a win or loss based on whether there are detectable squares left
+    # can identify a win or loss based on whether there are detectable squares
     if len(self.SQUARES) == 0:
-      outcome = self.generate_gutter_text('You Win!', 2)
-      outcome.draw(self.WINDOW)
+      self.generate_gutter_text('You Win!', 2).draw(self.WINDOW)
     else:
       outcome = self.generate_gutter_text('You Lose!', 2)
       outcome.draw(self.WINDOW)
     self.WINDOW.getMouse()
 
+  # TODO
+  def play_again_buttons (self):
+    buttons = [\
+      self.generate_gutter_text('Yes', 2, 0.5), \
+      self.generate_gutter_text('No', 2, 1.5)]
+    for button in buttons:
+      button.draw(self.WINDOW)
+
 def main():
-  game_instance = GameManager()
-  game_instance.game_loop()  
+  while(True):
+    game_instance = GameManager()
+    game_instance.boot()
+    game_instance.game_loop()  
     
 if __name__ == '__main__':
   main()
+
+"""
+TODO:
+  high score track + display
+  ask if play again
+  cut down on repetition in game_loop & am_in_square + contents_of_square
+"""
