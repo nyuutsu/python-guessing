3d54b49e3549feb6d3a9a7fbd9adf77b8a73e558
nyuu posts <nyuutsu@protonmail.com>
2022-11-26 11:34:23 -0500

    refactor, add fail timer, dynamic gutter text

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from graphics import *
 import math
 import random
 import string
+import time
 
 class GameManager:
   def __init__(self, width=500, height=500, squares_count=16, gap=50):
     # initialize arg constants
     self.WIDTH = width # window width
     self.HEIGHT = width # window height
+
     self.SQUARES_COUNT = squares_count
     self.GAP = gap
     
@@ -16,18 +18,27 @@ class GameManager:
     self.SQUARES_PER_ROW = int(math.floor(math.sqrt(self.SQUARES_COUNT)))
     self.MAX_SQUARE_WIDTH = self.WIDTH // self.SQUARES_PER_ROW
     self.HALF_SQUARE_WIDTH = self.MAX_SQUARE_WIDTH // 2
-    self.SQUARES = []
+    self.SQUARES = [] # should this be CONSTANTCAPPED or no
 
-    # initialize magic constants
+    # initialize hard constants
     self.FONT_SIZE = 30
     self.HALF_FONT_SIZE = self.FONT_SIZE // 2
     self.FONT_FACE = 'courier'
-    self.BG_COLOR = 'grey'
-    self.SQ_COLOR = 'lightgrey'
-    self.SQ_STROKE = 'purple'
+    self.BG_COLOR = color_rgb(0, 43, 54)
+    self.SQ_COLOR = color_rgb(253, 246, 227)
+    self.SQ_STROKE = color_rgb(108, 113, 196)
+    self.YES_STROKE = color_rgb(133, 153, 0)
+    self.MAYBE_STROKE = color_rgb(181, 137, 0)
+    self.NO_STROKE = color_rgb(211, 54, 130)
     self.SQ_STROKE_WIDTH = 5
+    # with correct play you only need 0.8n cards of guesses
+    # https://www.tandfonline.com/doi/abs/10.4169/amer.math.monthly.120.09.787
+    self.MAX_GUESSES = int(math.ceil(self.SQUARES_COUNT * 0.8))
+    self.remaining_guesses = self.MAX_GUESSES
+    self.FAIL_COOLDOWN = 1.5 # seconds
 
     # sanity check
+    # assert self.MAX_GUESSES == 13 # not true for all n, is true for n=16
     assert width == height
     assert width % 2 == 0
     assert gap % 2 == 0
@@ -37,13 +48,13 @@ class GameManager:
     # helper for create_one_square()
     def gen_secrets(squares_count):
       secrets = []
-      for char in string.ascii_letters:
+      for char in string.ascii_uppercase:
         secrets.append(char)
-        secrets.append(char) # we want these to come in pairs
+        secrets.append(char) # we want these to come in pairs. better way?
       truncated_secrets = secrets[0:squares_count]
       #random.shuffle(truncated_secrets) #turn off shuffle to test matching
       return iter(truncated_secrets)
-    self.secrets = gen_secrets(self.SQUARES_COUNT) # iterator!
+    self.secrets = gen_secrets(self.SQUARES_COUNT)
     
     # helper for create_one_row()
     def create_one_square(p1, p2):
@@ -73,39 +84,30 @@ class GameManager:
         displacement += self.MAX_SQUARE_WIDTH
     gen_squares()
 
-    # create window  
     def create_window():
       self.WINDOW = GraphWin("My Window", self.WIDTH, self.HEIGHT)
       self.WINDOW.setBackground(self.BG_COLOR)
     create_window()
-    
-    # end of constructor
 
-  # based on the initialization of the game object we should be able to draw all the squares. do so.
-  def draw_squares(self):
-    for obj in self.SQUARES:
-      obj[0].draw(self.WINDOW)
- 
-  # accepts a (assumed to be square) Rectangle object
-  # returns a Point representing the Rectangle's centerpoint
-  def get_center_of_this_square(self, square):
-    center = Point(\
-      square.getP1().getX() + self.HALF_SQUARE_WIDTH - self.HALF_GAP, \
-      square.getP1().getY() + self.HALF_SQUARE_WIDTH - self.HALF_FONT_SIZE)
-    return center
+    def draw_squares():
+      for obj in self.SQUARES:
+        obj[0].draw(self.WINDOW)
+    draw_squares()
 
-  # takes a "square tuple"
-  # notices where you clicked and draws the corresponding secret (tuple[1]) in the center of the containing square (tuple[0])
-  def unconditionally_permanently_reveal(self, data):
+  # takes (Rectangle, string)
+  # notices where you clicked and draws the corresponding secret (tuple[1]) in a Text at the center of the containing square (tuple[0])
+  # returns the Text
+  def reveal_and_return(self, data):
     # get the text we're printing
-    center = self.get_center_of_this_square(data[0])
+    center = data[0].getCenter()
     text = Text(center, data[1])
     text.setSize(self.FONT_SIZE)
     text.setFace(self.FONT_FACE)
+    text.setTextColor(self.BG_COLOR)
     text.draw(self.WINDOW)
     return text
 
-  # takes a Point object
+  # takes a Point
   # figures out whether the Point is within one of the squares in self.SQUARES
   # returns a boolean
   def am_in_square(self, data):
@@ -129,7 +131,7 @@ class GameManager:
   # takes a Point object
   # assumes Point is within one of the square tuples in self.SQUARES
   # returns that tuple
-  # I tried reworking this and am_in_square to use a decorator since they're like 99% the same function but I couldn't figure it out in a reasonable amount of time + I'm not sure doing it is useful for final design
+  # I tried reworking this and am_in_square to use a decorator since they're like 99% the same function but I didn't figure it out and moved on. revisit!
   def contents_of_square(self, data):
       # can I get out of having to write all this out?
       click_x = data.getX()
@@ -145,9 +147,9 @@ class GameManager:
         p2y = p2.getY()
 
         if click_x >= p1x and click_x <= p2x and click_y >= p1y and click_y <= p2y:
-          return square # if you're here, you found a square
+          return square
 
-  # listens for a click, looks at the list of [remaining] Rectangle, string tuples for a containing Rectangle, repeats this until match, returns tuple
+  # listens for a click, looks at the list of [remaining] (Rectangle, string) for a cmpatible Rectangle, repeats this until match, returns tuple
   def get_first_square(self):
     while(True):
       point_clicked = self.WINDOW.getMouse() # Point
@@ -164,32 +166,86 @@ class GameManager:
       if is_in_square == False:
         continue # must be in a square
       second_square = self.contents_of_square(point_clicked) # SqTup
-      if second_square[0] is first_square[0]:
+      if first_square[0] is second_square[0]:
         continue # must not be the same square
       return second_square
 
+  def meditate(self):
+    time.sleep(self.FAIL_COOLDOWN)
+
+  def generate_gutter_text(self, text, gutter_constant):
+    text = Text(\
+      Point(\
+        int(math.ceil(self.WIDTH / 2)), \
+        int(math.ceil(self.HEIGHT * (0.25 * gutter_constant)))), \
+        text)
+    text.setTextColor(self.SQ_COLOR)
+    text.setSize(self.FONT_SIZE)
+    return text
+
+  def game_loop(self):
+    
+    title_text = self.generate_gutter_text(f'{self.SQUARES_COUNT} tiles! Match them!', 1)
+    title_text.draw(self.WINDOW)
+    # while user has guesses and guessables
+    while(self.remaining_guesses > 0 and len(self.SQUARES) != 0):
+      # display currently remaining guesses
+      guesses_text = self.generate_gutter_text(f'Guesses remaining: {self.remaining_guesses}', 3)
+      guesses_text.draw(self.WINDOW)
+
+      # prompt for a valid guess; reveal
+      square_one = self.get_first_square()
+      text_one = self.reveal_and_return(square_one)
+      square_one[0].setOutline(self.MAYBE_STROKE)
+      # add a fail timer?
+      # prompt for a valid other guess; reveal
+      square_two = self.get_second_square(square_one)
+      text_two = self.reveal_and_return(square_two)
+
+      """
+      interpret results:
+        match:
+          leave match text revealed, discard Text references
+            we can no longer hide the text, which is good!
+          outline match Rectangle borders, discard (Rectangle, text) references
+            we can no longer detect these Rectangles, simplifying our square-matching algorithm
+            we can use whether any detectable (Rectangle, text)s to check wins
+        nomatch:
+          outline nomatch Rectangle borders as incorrect
+          allow player to meditate briefly
+          hide unmatched Texts
+          outline nomatch Rectangle borders as neutral
+        finally:
+          deduct a guess
+      """
+      if square_one[1] == square_two[1]: # match
+        square_one[0].setOutline(self.YES_STROKE)
+        square_two[0].setOutline(self.YES_STROKE)
+        self.SQUARES.remove(square_one)
+        self.SQUARES.remove(square_two)
+      else: # nomatch
+        square_one[0].setOutline(self.NO_STROKE)
+        square_two[0].setOutline(self.NO_STROKE) 
+        self.meditate() # reflect on your choices for three seconds
+        square_one[0].setOutline(self.SQ_STROKE)
+        square_two[0].setOutline(self.SQ_STROKE) # writing these calls twice is a sign I should somehow be avoiding this
+        text_one.undraw()
+        text_two.undraw()
+      self.remaining_guesses -= 1
+      guesses_text.undraw()
+      #print(f'remaining guesses: {self.remaining_guesses}')
+    # if either loop conditions are broken then we can identify a win or loss based on whether there are detectable squares left
+    if len(self.SQUARES) == 0:
+      outcome = self.generate_gutter_text('You Win!', 2)
+      outcome.draw(self.WINDOW)
+    else:
+      outcome = self.generate_gutter_text('You Lose!', 2)
+      outcome.draw(self.WINDOW)
+    self.WINDOW.getMouse()
+
 def main():
   game_instance = GameManager()
-  game_instance.draw_squares()
-
-  while(True):
-    square_one = game_instance.get_first_square()
-    text_one = game_instance.unconditionally_permanently_reveal(square_one)
-    square_two = game_instance.get_second_square(square_one)
-    text_two = game_instance.unconditionally_permanently_reveal(square_two)
-    if square_one[1] == square_two[1]:
-      square_one[0].setOutline('green')
-      square_two[0].setOutline('green')
-      game_instance.SQUARES.remove(square_one)
-      game_instance.SQUARES.remove(square_two)
-    else:
-      square_one[0].setOutline('red')
-      square_two[0].setOutline('red')
-      game_instance.WINDOW.getMouse()
-      square_one[0].setOutline('purple')
-      square_two[0].setOutline('purple')
-      text_one.undraw()
-      text_two.undraw()
-
+  game_instance.game_loop()  
+    
 if __name__ == '__main__':
   main()
